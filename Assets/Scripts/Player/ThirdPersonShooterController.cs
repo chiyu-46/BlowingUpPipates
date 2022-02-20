92a2b4073fad16b6bdb7e5ae204997c619034aa2
chiyu-46 <mrchengxinda@outlook.com>
2022-02-20 16:03:13 +0800

    实现Player IK

--- Assets/Scripts/Player/ThirdPersonShooterController.cs
+++ Assets/Scripts/Player/ThirdPersonShooterController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using Custom.FinalIK;
 using UnityEngine;
 using Unity.Netcode;
 
@@ -19,6 +20,12 @@ public class ThirdPersonShooterController : NetworkBehaviour
     private Transform pfBulletProjectile;
     [SerializeField] [Tooltip("子弹生成位置，即枪口位置")]
     private Transform spawnBulletPosition;
+
+    [Header("Aim IK")]
+    [SerializeField][Tooltip("瞄准位置")]
+    private Transform aimTarget;
+    [SerializeField][Tooltip("瞄准控制器组件")]
+    private CustomAimController aimController;
     
     private InputHandler _input;
     private ThirdPersonController thirdPersonController;
@@ -28,11 +35,6 @@ public class ThirdPersonShooterController : NetworkBehaviour
     /// 鼠标指向位置对应的世界空间位置。
     /// </summary>
     private Vector3 mouseWorldPosition = Vector3.zero;
-    /// <summary>
-    /// 场景中用于显示瞄准位置的粒子效果。
-    /// </summary>
-    private Transform debugFX;
-    
     
     
     private void Start()
@@ -40,10 +42,9 @@ public class ThirdPersonShooterController : NetworkBehaviour
         _input = GetComponent<InputHandler>();
         thirdPersonController = GetComponent<ThirdPersonController>();
         TryGetComponent(out _animator);
-        aimVirtualCamera = GameObject.Find("PlayerAimCamera").GetComponent<CinemachineVirtualCamera>();
-        debugFX = GameObject.Find("FX_GlowSpot_03").GetComponent<Transform>();
         if (IsClient && IsOwner)
         {
+            aimVirtualCamera = GameObject.Find("PlayerAimCamera").GetComponent<CinemachineVirtualCamera>();
             aimVirtualCamera.Follow = thirdPersonController.CinemachineCameraTarget.transform;
         }
     }
@@ -55,7 +56,7 @@ public class ThirdPersonShooterController : NetworkBehaviour
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
         if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask)) {
             mouseWorldPosition = raycastHit.point;
-            debugFX.position = raycastHit.point;
+            aimTarget.position = raycastHit.point;
         }
         if (_input.aim)
         {
@@ -64,19 +65,21 @@ public class ThirdPersonShooterController : NetworkBehaviour
             thirdPersonController.SetRotateOnMove(false);
             
             //Todo: 配合IK制作更加自然的动作。
-            // 确定瞄准方向，在瞄准状态下，Player始终朝向此方向
-            Vector3 worldAimTarget = mouseWorldPosition;
-            worldAimTarget.y = transform.position.y;
-            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
-            
-            // 使Player（平滑地）朝向瞄准方向
-            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
+            aimController.target = aimTarget;
+            // // 确定瞄准方向，在瞄准状态下，Player始终朝向此方向
+            // Vector3 worldAimTarget = mouseWorldPosition;
+            // worldAimTarget.y = transform.position.y;
+            // Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+            //
+            // // 使Player（平滑地）朝向瞄准方向
+            // transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
         }
         else
         {
             aimVirtualCamera.enabled = false;
             thirdPersonController.SetSensibility(normalSensitivity);
             thirdPersonController.SetRotateOnMove(true);
+            aimController.target = null;
         }
         
         if (_input.shoot) {
